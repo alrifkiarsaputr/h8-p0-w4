9f7c2d10a56cd0322225b8ca19b4300a81f072dc
Alrifki Arsa Putra <putraalrifki@gmail.com>
2018-09-25 17:46:38 +0700

    revisi exercises 6 hitung huruf

--- Exercises_6.js
+++ Exercises_6.js
@@ -1,26 +1,38 @@
-function hitungHuruf(kata){
-var pecahArray=kata.split(' ')//[ 'rajin', 'pangkal', 'kaya' ]
-
-var i=0
-while(i<pecahArray.length){
-    var j=0
-    while(j<pecahArray[i].length){
-        var k=0
-        var jumlah=0
-        while(k<pecahArray[i].length){
-            if(pecahArray[i][j]===pecahArray[i][k]){
-                jumlah=jumlah+1
-            }
-            k++
+function hitungHuruf(kata) {
+    var pisah = kata.split(' ');
+    var kataPerulangan = []; //Tampung total perulangan di setiap kata
+    var ulangHuruf = [];
+    // return pisah
+    for(var i = 0; i < pisah.length; i++){
+      var pisahHuruf = pisah[i].split(''); //bikin array dari 1 kata
+      // return pisahHuruf
+      var hurufPerulangan = 0; //Tampung total ulang di setiap huru dalam 1 kata
+      for(var j = 0; j < pisah[i].length -1; j++){
+        for(var k = j; k < pisahHuruf.length; k++){
+          
+          if(j !== k && pisah[i][j] === pisahHuruf[k]){
+            hurufPerulangan += 1;//identifikasi huruf yang sama, klo sama kasih point 1
+            var splice = pisahHuruf.splice(k, 1);
+            
+          }
+          
         }
-        
-        j++
+      }
+      kataPerulangan.push(hurufPerulangan);
     }
-    i++
-    return pecahArray[i]
-}
-
-}
-
-console.log(hitungHuruf('rajin pangkal kaya'));
-console.log(hitungHuruf('mengayuh perahu di danau')); // danau
+    
+    var perulanganTerbesar = Math.max.apply(null, kataPerulangan); //Cari angka yang paling gede
+    var lokasiPerulanganTerbesar = kataPerulangan.indexOf(perulanganTerbesar);//Cari lokasi array dari array kata yang udah di split
+    if(lokasiPerulanganTerbesar !== 0){
+      return pisah[lokasiPerulanganTerbesar];
+    }else{
+      return -1;
+    }
+  }
+  
+  // TEST CASES
+  console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
+  console.log(hitungHuruf('I am a passionate developer')); // passionate
+  console.log(hitungHuruf('aku adalah anak gembala')); // adalah
+  console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
+  console.log(hitungHuruf('mengayuh perahu di danau')); // danau
